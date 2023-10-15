bcaeef92f039c7a1a0f20179125d4e4fd9aa05a6
Shreyas Ashtamkar <51398602+Shreyas-Ashtamkar@users.noreply.github.com>
2023-10-15 19:16:26 +0530

    Added Chatbot, integrated Chat Engine

--- app.py
+++ app.py
@@ -1,14 +1,40 @@
-from flask import Flask, request
+from flask import Flask, request, render_template
+from whatsapp import WhatsappMessage
+from emoji import demojize
+
+from chatengine import ChatEngine
+
 app = Flask(__name__)
 
-@app.route("/chatbot", methods = ['GET', 'POST'])
+@app.route("/", methods = ["GET"])
 def index():
+    return render_template("index.html")
+
+@app.route("/chatbot", methods = ['GET', 'POST'])
+def chatbot():
     if request.method == "GET":
-        action = request.args.get("action")
-        if action == "convert":
-            print("Calling Unitconverter")
-        else:
-            print("Calling some other function")
-            
+        message = {
+            "From" : "Web",
+            "Body" : request.args.get("action"),
+            "To"   : "Web"
+        }
+    elif request.method == "POST":
+        message = {
+            "From" : request.form.get("From"),
+            "Body" : demojize(request.form.get("Body")),
+            "To"   : request.form.get("To")
+        }
+
+        action = ChatEngine.parse(message["Body"])
+        output = ChatEngine.execute(action)
+
+        wm = WhatsappMessage(
+            src  = message["To"],
+            body = output,
+            dest = message["From"]
+        )
+        wm.send()
+    
+    print(message)
 
     return "Hello World"
